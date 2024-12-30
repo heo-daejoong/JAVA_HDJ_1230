87395b5579e9556b92a247304fec7411fb8887a7
heo-daejoong <dygks5787@naver.com>
2024-12-30 16:13:01 +0900

    자바 1일차 : 변수 선언 예제

---
+++ java_hdj/src/day01/Ex05_VariableBoolean.java
@@ -0,0 +1,14 @@
+package day01;
+
+public class Ex05_VariableBoolean {
+
+	public static void main(String[] args) {
+		
+		boolean isAdult = true; // 또는 false
+		int age = 10;
+		isAdult = age > 18; // 19세 이상이면 성인
+		System.out.println(age + "살은 성인? " + isAdult);
+		
+	}
+
+}
