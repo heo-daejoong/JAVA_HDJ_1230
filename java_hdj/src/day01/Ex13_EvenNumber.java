6a274deed8037723f39c53a87d4a90889beb94ec
heo-daejoong <dygks5787@naver.com>
2024-12-30 17:32:52 +0900

    자바 1일차 : 오퍼레이터

---
+++ java_hdj/src/day01/Ex13_EvenNumber.java
@@ -0,0 +1,24 @@
+package day01;
+
+public class Ex13_EvenNumber {
+
+	public static void main(String[] args) {
+		
+		/* 주어진 num가 짝수이면 true를, 아니면 false가 출력되도록 코드를 작성하세요.
+		 * 출력 예시 1
+		 * 3은 짝수인가? false
+		 * 출력 예시 2
+		 * 4는 짝수인가? true
+		 */
+		
+		int num = 4;
+		//num을 2로 나누었을때 나머지가 0과 같다
+		boolean isEven = (num % 2 == 0);
+		System.out.println(num + "는 짝수와 같다." + isEven );
+		
+		
+		
+		
+	}
+
+}
