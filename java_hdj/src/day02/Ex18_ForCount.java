d676a843c38148fc977a90acfa7c282d0aedbe7e
heo-daejoong <dygks5787@naver.com>
2024-12-31 17:06:36 +0900

    자바 2일차 : For문 예제 연습

---
+++ java_hdj/src/day02/Ex18_ForCount.java
@@ -0,0 +1,20 @@
+package day02;
+
+public class Ex18_ForCount {
+
+	public static void main(String[] args) {
+		/* 1부터 5까지 출력하는 코드를 작성해보세요.(for문)
+		 * 반복횟수 : i는 1부터 5까지 1씩 증가
+		 * 규칙석 : i를 출력
+		 * 반복문 종료 후 : 없음
+		 * */
+		int i;
+		for( i = 1 ; i <= 5 ; i++ ) {
+			System.out.println(i);
+		}
+		
+		
+		
+	}
+
+}
---
+++ java_hdj/src/day02/Ex19_ForMultipleTable.java
@@ -0,0 +1,27 @@
+package day02;
+
+public class Ex19_ForMultipleTable {
+
+	public static void main(String[] args) {
+		/* 2단을 출력하는 코드를 작성하세요
+		 * 반복횟수 : i가 2부터 18까지 2씩 증가
+		 * 규칙성 : i를 출력
+		 * 반복문 종료 후 : 없음
+		 * */
+		/*int num1 = 2, num2;
+		for( num2 = 1; num2 <= 9 ; num2++ ) {
+			System.out.println(num1 + " X " + num2 + " = " + (num1 * num2));
+		}*/
+		
+		int num = 2;
+		
+		for(int i = 1 ; i <= 9 ; i++) {
+			System.out.println(num + " X " + i + " = " + i * 2);
+		}
+		
+		
+		
+		
+	}
+
+}
