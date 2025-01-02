cc6161c79d6264c7547a1815ae2522a868ff592d
heo-daejoong <dygks5787@naver.com>
2025-01-02 16:24:40 +0900

    자바 3일차 : 중첩 For문 예제

---
+++ java_hdj/src/day03/Ex18_MultipleTable.java
@@ -0,0 +1,37 @@
+package day03;
+
+public class Ex18_MultipleTable {
+
+	public static void main(String[] args) {
+		/* 2 ~ 9단을 출력하는 코드를 작성하세요
+		 * 2단
+		 * 2 X 1 = 2
+		 * ...
+		 * 3단
+		 * ...
+		 * 9단
+		 * ...
+		 * 9 X 9 = 81
+		 * 
+		 * 반복횟수 : num는 2부터 9까지 1씩 증가
+		 * 규칙성 : num단 출력
+		 * 반복문 종료 후 : 없음
+		 * */
+		
+		int num = 2;
+		for(num = 2; num <= 9; num++) {
+			System.out.println(num + "단");
+			for(int i = 1 ;i <= 9 ;i++ ) {
+				System.out.println(num + " X " + i +  " = " + num * i);
+				
+			
+		}
+		
+		
+				
+			}
+		}
+		
+	}
+
+
