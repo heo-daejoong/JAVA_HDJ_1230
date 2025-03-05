e1cc5ebc66852d6e3bf19994ebe516bb06d11df7
heo-daejoong <dygks5787@naver.com>
2025-03-05 10:10:13 +0900

    DB 1일차

---
+++ java_hdj/src/db/ex1/dao/SubjectDAO.java
@@ -0,0 +1,15 @@
+package db.ex1.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import db.ex1.model.vo.SubjectVO;
+
+public interface SubjectDAO {
+	
+	List<SubjectVO> selectSubjectList();
+
+
+
+}
