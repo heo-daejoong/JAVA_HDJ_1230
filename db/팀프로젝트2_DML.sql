239fc2069a289cec41fc9b18cbb7c448c2501b77
heo-daejoong <dygks5787@naver.com>
2025-03-19 16:13:57 +0900

    스프링 03일차 : spring - 게시판 등록 완료

---
+++ db/팀프로젝트2_DML.sql
@@ -0,0 +1,31 @@
+INSERT INTO PROGRAM_AGE(PA_AGE) VALUES
+("모든 연령 시청가"),
+("7세 이상 시청가"),
+("12세 이상 시청가"),
+("15세 이상 시청가"),
+("19세 이상 시청가");
+
+INSERT INTO WEEK(WE_KEY, WE_NAME) VALUES
+(1, "일요일"),
+(2, "월요일"),
+(3, "화요일"),
+(4, "수요일"),
+(5, "목요일"),
+(6, "금요일"),
+(7, "토요일");
+
+INSERT INTO GENRE(GR_NAME) VALUES
+("예능"),
+("드라마"),
+("시사"),
+("뉴스"),
+("문화"),
+("사회"),
+("음악"),
+("여행"),
+("자연");
+
+INSERT INTO CHANNEL(CH_NAME, CH_NUM) VALUES
+("KBS", 7), ("MBC", 11), ("SBS", 5);
+
+
