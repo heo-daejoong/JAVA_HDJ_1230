1c8ceafbb41b286e75bdb9b72ed455819fd413a4
heo-daejoong <dygks5787@naver.com>
2025-04-09 16:37:20 +0900

    리액트 2일차

---
+++ react/react2/src/Button.js
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+
+function Button({click, className, text="버튼"}){
+	
+	return(
+		<button onClick={click} className={className}>{text}</button>
+	);
+}
+
+export default Button;
