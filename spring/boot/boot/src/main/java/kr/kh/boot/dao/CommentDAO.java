7d45732391b389100b818c099d157c3ae455ed6d
heo-daejoong <dygks5787@naver.com>
2025-04-24 14:51:29 +0900

    스프링 부트 : 4일차 - 댓글기능

--- spring/boot/boot/src/main/java/kr/kh/boot/dao/CommentDAO.java
+++ spring/boot/boot/src/main/java/kr/kh/boot/dao/CommentDAO.java
@@ -1,9 +1,16 @@
 package kr.kh.boot.dao;
 
+import java.util.List;
+
 import kr.kh.boot.model.vo.CommentVO;
+import kr.kh.boot.utils.Criteria;
 
 public interface CommentDAO {
 
 	boolean insertComment(CommentVO comment);
+
+	List<CommentVO> selectCommentList(Criteria cri);
+
+	int selectCountCommentList(Criteria cri);
 	
 }
