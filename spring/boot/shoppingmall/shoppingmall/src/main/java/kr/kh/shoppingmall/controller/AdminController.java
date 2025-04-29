8eaefc40463db2d66c252683945edc7eee088068
heo-daejoong <dygks5787@naver.com>
2025-04-29 14:18:16 +0900

    쇼핑몰 : 카테고리 등록

--- spring/boot/shoppingmall/shoppingmall/src/main/java/kr/kh/shoppingmall/controller/AdminController.java
+++ spring/boot/shoppingmall/shoppingmall/src/main/java/kr/kh/shoppingmall/controller/AdminController.java
@@ -1,18 +1,41 @@
 package kr.kh.shoppingmall.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import kr.kh.shoppingmall.model.vo.CategoryVO;
+import kr.kh.shoppingmall.service.ProductService;
+
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+
+	@Autowired
+	ProductService productService;
 	
 	@GetMapping("/page")
 	public String page() {
 		return "admin/page";
+	}
+	
+	@GetMapping("/category")
+	public String category(Model model) {
+		List<CategoryVO> list = productService.getCategory();
+		model.addAttribute("list", list);
+		return "admin/category";
+	}
+	
+	@GetMapping("/category/insert")
+	@ResponseBody
+	public String categoryInsert(CategoryVO category) {
+		return productService.insertCategory(category);
 	}
 	
 }
