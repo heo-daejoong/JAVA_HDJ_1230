54a2f0144bc5fa4108dc3851c64cb797797fd274
heo-daejoong <dygks5787@naver.com>
2025-04-30 12:41:16 +0900

    쇼핑몰 : 헤더 이미지 경로 수정

--- spring/boot/shoppingmall/shoppingmall/src/main/java/kr/kh/shoppingmall/config/WebMvcConfig.java
+++ spring/boot/shoppingmall/shoppingmall/src/main/java/kr/kh/shoppingmall/config/WebMvcConfig.java
@@ -17,6 +17,6 @@ public class WebMvcConfig implements WebMvcConfigurer{
 	
 	public void addResourceHandlers(ResourceHandlerRegistry registry){
 		registry.addResourceHandler("/file/**").addResourceLocations("file:///"+uploadPath);
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/");
 	}
 }
